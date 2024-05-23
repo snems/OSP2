60218f9dd551854702f5696e618732b063adf4cf
Alexandr Bushuev <zxsnems@gmail.com>
2024-05-23 15:22:10 +0500

    version, changelog

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -1969,7 +1969,7 @@ int CG_NewParticleArea(int num);
 
 qboolean CG_DrawIntermission(void);
 /*************************************************************************************************/
-#define OSP_VERSION "0.01-test.002"
+#define OSP_VERSION "0.01-test.003"
 
 
 //
