56202f0a8464f65442da01ddafcf637db805485d
Alexandr Bushuev <zxsnems@gmail.com>
2024-05-31 12:10:55 +0500

    cg_thirdPreson usage restricted

--- code/cgame/cg_view.c
+++ code/cgame/cg_view.c
@@ -929,7 +929,7 @@ void CG_DrawActiveFrame(int serverTime, stereoFrame_t stereoView, qboolean demoP
 	CG_PredictPlayerState();
 
 	// decide on third person view
-	cg.renderingThirdPerson = cg_thirdPerson.integer || (cg.snap->ps.stats[STAT_HEALTH] <= 0);
+	cg.renderingThirdPerson = (cg_thirdPerson.integer && cg.demoPlayback) || (cg.snap->ps.stats[STAT_HEALTH] <= 0);
 
 	// build cg.refdef
 	inwater = CG_CalcViewValues();
