ca9b41abcdad9e4c5336476acf491c743d9bb80f
Alexandr Bushuev <zxsnems@gmail.com>
2024-07-22 15:24:55 +0500

    Decorator fixed

--- code/cgame/cg_superhud_element_pred.c
+++ code/cgame/cg_superhud_element_pred.c
@@ -46,14 +46,14 @@ void CG_SHUDElementDecorRoutine(void* context)
 		else if (element->ctx.image)
 		{
 			trap_R_SetColor(element->ctx.color);
-			trap_R_DrawStretchPic(element->ctx.x, element->ctx.y, element->ctx.w, element->ctx.h, 0, 0, 0, 0, element->ctx.image);
+			trap_R_DrawStretchPic(element->ctx.x, element->ctx.y, element->ctx.w, element->ctx.h, 0, 0, 1, 1, element->ctx.image);
 			trap_R_SetColor(NULL);
 		}
 	}
 	else if (element->ctx.image)
 	{
 		trap_R_SetColor(element->ctx.color);
-		trap_R_DrawStretchPic(element->ctx.x, element->ctx.y, element->ctx.w, element->ctx.h, 0, 0, 1, 1, element->ctx.image);
+		trap_R_DrawStretchPic(element->ctx.x, element->ctx.y, element->ctx.w, element->ctx.h, 0, 0, 0, 0, element->ctx.image);
 		trap_R_SetColor(NULL);
 	}
 }
