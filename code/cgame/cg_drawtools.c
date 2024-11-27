acd5e6543cfa145d2f888cdef6f175d8e8e5a4e6
Alexandr Bushuev <zxsnems@gmail.com>
2024-11-27 21:11:29 +0500

    New font eternal

--- code/cgame/cg_drawtools.c
+++ code/cgame/cg_drawtools.c
@@ -609,7 +609,7 @@ typedef struct
 	int             shaderCount;
 } font_t;
 
-static font_t fonts[] = {{"id"}, {"idblock"}, {"sansman"}, {"cpma"}, {"m1rage"}, {"elite_emoji"}, {"diablo"}};
+static font_t fonts[] = {{"id"}, {"idblock"}, {"sansman"}, {"cpma"}, {"m1rage"}, {"elite_emoji"}, {"diablo"}, {"eternal"},};
 static int fonts_num = sizeof(fonts) / sizeof(fonts[0]);
 static const font_t* font = &fonts[0];
 static const font_metric_t* metrics = &fonts[0].metrics[0];
@@ -918,6 +918,7 @@ void CG_LoadFonts(void)
 	CG_LoadFont(&fonts[4], "gfx/2d/m1rage.cfg");
 	CG_LoadFont(&fonts[5], "gfx/2d/elite_emoji.cfg");
 	CG_LoadFont(&fonts[6], "gfx/2d/diablo.cfg");
+	CG_LoadFont(&fonts[7], "gfx/2d/eternal.cfg");
 }
 
 
