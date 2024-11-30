57771bee42c6890781b0a3313039931e52552cb0
diwoc <108992394+scoqx@users.noreply.github.com>
2024-11-30 14:46:27 +0300

    Added cg_drawGun 3/4, cg_gunOpaque, cg_gunColor
    cg_drawGun 3 - 1st person view individual shader for weapons
    cg_drawGun 4 - same as 3, but weapons are static (like cg_drawGun 2)
    cg_gunOpaque - set transparent for cg_drawGun 3/4
    cg_gunColor - set color for cg_drawGun 3/4

--- code/cgame/cg_localevents.c
+++ code/cgame/cg_localevents.c
@@ -399,6 +399,9 @@ void CG_LocalEventCvarChanged_cg_dlightBFG(cvarTable_t* cvart)
 	CG_UpdateWeaponDlightColor(WP_BFG);
 }
 
-
+void CG_LocalEventCvarChanged_cg_gunColor(cvarTable_t* cvart)
+{
+	void CG_UpdateGunShaderRGBA(refEntity_t* gun);
+}
 
 
