32a0946cdf899fb0e4941531d18f90f4250ecc7e
Aleksandr Bushuev <al.bushuev@yadro.com>
2025-03-31 20:25:08 +0500

    Revert "Sound "Fight" for freeze rounds"
    This reverts commit 8db953be90acedeb89a3a3b2829926309e812932.

--- code/cgame/cg_superhud.c
+++ code/cgame/cg_superhud.c
@@ -292,30 +292,6 @@ void CG_SHUDLoadConfig(void)
 	}
 }
 
-void CG_SHUDFreezeFight(void)
-{
-	static qboolean is_spectator = qfalse;
-	const clientInfo_t *ourPlayer;
-	qboolean current_spec;
-
-	if (cgs.osp.gameTypeFreeze == 0)
-	{
-		is_spectator = qfalse;
-		return;
-	}
-
-	ourPlayer = &cgs.clientinfo[cg.clientNum];
-	current_spec = ourPlayer->rt == TEAM_SPECTATOR || ourPlayer->team == TEAM_SPECTATOR || cg.clientNum != cg.snap->ps.clientNum || ourPlayer->health <= 0;
-
-	if (is_spectator && !current_spec)
-	{
-			trap_S_StartLocalSound(cgs.media.countFightSound, CHAN_ANNOUNCER);
-	}
-
-	is_spectator = current_spec;
-
-}
-
 void CG_SHUDRoutine(void)
 {
 	superhudElement_t* last = elementsHead;
@@ -332,8 +308,6 @@ void CG_SHUDRoutine(void)
 	{
 		CG_OSPDrawCenterString();
 	}
-
-	CG_SHUDFreezeFight();
 
 	while (last)
 	{
