3a7bbdd06faecaa4ac4c0c2b8edf9a9e807624b5
metaq3 <ruslanbez2006@gmail.com>
2026-06-17 14:22:15 +0500

    chore: cleanup configuration for gnu-like compilers

--- cmake/compilers/gnu.cmake
+++ cmake/compilers/gnu.cmake
@@ -4,13 +4,20 @@ if(NOT CMAKE_C_COMPILER_ID STREQUAL "GNU" AND NOT CMAKE_C_COMPILER_ID MATCHES "^
     return()
 endif()
 
-add_compile_options(-Wall -Wimplicit -Wshadow
-    -Werror -lmvec -lm -Wl,--no-undefined
+add_compile_options(-Wall -Wimplicit -Wshadow -Werror
     -Wstrict-prototypes -Wformat=2  -Wformat-security
     -Wstrict-aliasing=2 -Wmissing-format-attribute
     -Wdisabled-optimization -Werror-implicit-function-declaration)
 
 add_compile_options(-Wno-format-zero-length -Wno-format-nonliteral)
+
+# lmvec is for vectorized maths
+add_compile_options(-lmvec -lm)
+
+# We must have all symbols defined. Without this, successful compilation
+# may produce unusable binary, that cannot load because of some missing
+# function
+add_compile_options(-Wl,--no-undefined)
 
 # There are lots of instances of union based aliasing in the code
 # that rely on the compiler not optimising them away, so disable it
